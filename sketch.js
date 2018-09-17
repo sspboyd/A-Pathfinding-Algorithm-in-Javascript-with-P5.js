a57e71c68436e2974b3620d34e6fe8748e37b391
sspboyd <sspboyd@gmail.com>
2018-09-17 17:50:03 -0400

    I'm trying to help myself understand what's happening here instead of just following along with Shiffman in the video. To that end I've added in _a lot_ of comments. It's help identify where I'm not entirely clear about how thing work. Still more comments to add in for the Spot object, global vars and setup() function.

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ function removeFromArray(arr, elt) {
 
 function heuristic(a, b) {
     // var d = dist(a.i, a.j, b.i, b.j);
-    var d = abs(a.i-b.i) +abs(a.j-b.j);
+    var d = abs(a.i - b.i) + abs(a.j - b.j);
     return d;
 }
 
-cols = 29;
-rows = 29;
+cols = 47;
+rows = 47;
 
 var grid = new Array(cols);
 
@@ -36,41 +36,56 @@ function Spot(i, j) {
     this.previous = undefined;
     this.wall = false;
 
-    if(random(1)<0.3){
+    if (random(1) < 0.33) {
         this.wall = true;
     }
 
 
     this.show = function(col) {
         fill(col);
-        if(this.wall){
+        if (this.wall) {
             fill(color(0));
         }
         noStroke();
-        rect(this.i * w+1, this.j * h+1, w - 2, h - 2);
+        rect(this.i * w + 1, this.j * h + 1, w - 2, h - 2);
     }
 
     this.addNeighbours = function(grid) {
         this.i = i;
         this.j = j;
+        var tn = this.neighbours;
         if (i < cols - 1) {
-            this.neighbours.push(grid[i + 1][j]);
+            tn.push(grid[i + 1][j]);
         }
         if (i > 0) {
-            this.neighbours.push(grid[i - 1][j]);
+            tn.push(grid[i - 1][j]);
         }
         if (j < rows - 1) {
-            this.neighbours.push(grid[i][j + 1]);
+            tn.push(grid[i][j + 1]);
         }
         if (j > 0) {
-            this.neighbours.push(grid[i][j - 1]);
+            tn.push(grid[i][j - 1]);
+        }
+
+        //diagonals
+        if (i > 0 && j > 0) { // top left
+            tn.push(grid[i - 1][j - 1]);
+        }
+        if (i > 0 && j < rows - 1) { // top right
+            tn.push(grid[i - 1][j + 1]);
+        }
+        if (i < cols - 1 && j > 0) { // bottom left
+            tn.push(grid[i + 1][j - 1]);
+        }
+        if (i < cols - 1 && j < rows - 1) { // bottom right
+            tn.push(grid[i + 1][j + 1]);
         }
     }
 }
 
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 400);
     console.log("A*");
 
     w = width / cols;
@@ -93,73 +108,85 @@ function setup() {
     }
     start = grid[0][0];
     end = grid[cols - 1][rows - 1];
+    start.wall = false;
+    end.wall = false;
 
     openSet.push(start);
 }
 
 function draw() {
-    background(47);
+    background(0);
 
-    if (openSet.length > 0) {
+    if (openSet.length > 0) { // is there anything in the openSet, ie unchecked
         var winner = 0;
-        for (var i = 0; i < openSet.length; i++) {
+        for (var i = 0; i < openSet.length; i++) { // find the Spot with the lowest f and call it winner
             if (openSet[i].f < openSet[winner].f) {
                 winner = i;
             }
         }
         // keep going
-        var current = openSet[winner];
-        if (current === end) {
+        var current = openSet[winner]; // call the 'winner' Spot 'current'
+        if (current === end) { // are we at the end? If yes, then done!
             noLoop();
             console.log("Done!");
         }
 
-        removeFromArray(openSet, current);
-        closedSet.push(current);
+        removeFromArray(openSet, current); // so, not at the 'end' and we've evaluated this Spot. Take it out of the openSet
+        closedSet.push(current); // and add it to the Spots that have been checked
 
-        var neighbours = current.neighbours;
-        for (var i = 0; i < neighbours.length; i++) {
+        // Lets look at 'current' Spot's neighbours
+        var neighbours = current.neighbours; // make a new var and store "current" Spot's neighbours in "neighbours"
+        for (var i = 0; i < neighbours.length; i++) { // go through each of "current" Spot's neighbours
             var neighbour = neighbours[i];
-            if (!closedSet.includes(neighbour) && !neighbour.wall) {
-                var tempG = current.g + 1;
-                if (openSet.includes(neighbour)) {
-                    if (tempG < neighbour.g) {
+            if (!closedSet.includes(neighbour) && !neighbour.wall) { // check and see if this neighbour is valid, as in it hasn't been check before and it isn't a wall
+                var tempG = current.g + 1; // add an additional "step" to the g value. This basically means it is one step further away from the "start" Spot
+                if (openSet.includes(neighbour)) { // check and see if this "neighbour" Spot is in the openSet
+                    if (tempG < neighbour.g) { // im getting confused here.
                         neighbour.g = tempG;
                     }
                 } else {
                     neighbour.g = tempG;
                     openSet.push(neighbour);
                 }
-                neighbour.h = heuristic(neighbour, end);
-                neighbour.f = neighbour.g + neighbour.h;
-                neighbour.previous = current;
+                neighbour.h = heuristic(neighbour, end); // figure out the h value for this neighbour using the heuristic
+                neighbour.f = neighbour.g + neighbour.h; // add up g+h to get the f value for this Spot
+                neighbour.previous = current; // now set the "current" Spot as the 'previous' (parent) for this neighbour Spot
             }
-        }
+        } // finish looping through the rest of the "current" Spot neighbours
     } else {
-        //no solution
+        //no solution. Nothing left in the openSet. No Spot ended up being the "end" Spot
+        console.log("No Solution!");
+        noLoop(); // stop the draw function
+        return; // not exactly sure what this is for.
     }
 
+    // cycle through every spot in the grid and display the spot with the specified colour
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
             grid[i][j].show(color(255));
         }
     }
+
+    // go through all the closedSet Spots and colour them the specified colour (redish here)
     for (var i = 0; i < closedSet.length; i++) {
         closedSet[i].show(color(199, 76, 123));
     }
+
+    // go through all the openSet Spots and colour them the specified colour (greenish here)
     for (var i = 0; i < openSet.length; i++) {
         openSet[i].show(color(76, 199, 123));
     }
 
-    // find the path
-    path = [];
-    var temp = current;
-    path.push(temp);
-    while (temp.previous) {
-        path.push(temp.previous);
-        temp = temp.previous;
+    // Now colour the path found so far
+    path = []; // create new array of Spots from the "current" Spot connecting back to the "start" Spot
+    var temp = current; // create a var 'temp' to begin working backwards from
+    path.push(temp); // add this 'temp' Spot to the path array
+    while (temp.previous) { // as long as the 'temp' Spot has a value for 'previous', run this loop
+        path.push(temp.previous); // add the Spot in temp.previous to the path array
+        temp = temp.previous; // now make temp.previous the new temp
     }
 
+    // go through all the path Spots and colour them the specified colour (blueish here)
     for (var i = 0; i < path.length; i++) {
         path[i].show(color(76, 123, 199));
     }
