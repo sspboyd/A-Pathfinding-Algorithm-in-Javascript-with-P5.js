bf501f2f96348769910ad4af6e97c9812e07dc30
sspboyd <sspboyd@gmail.com>
2018-09-16 21:39:34 -0400

    Working! Changed from using a euclidean distance algorithm to a taxicab algorithm. Showing the path. Moved the start and end points to see how it changes things. Working well. Next up is to add some walls and diagonal movement.

--- sketch.js
+++ sketch.js
@@ -6,8 +6,15 @@ function removeFromArray(arr, elt) {
     }
 }
 
-cols = 5;
-rows = 5;
+
+function heuristic(a, b) {
+    // var d = dist(a.i, a.j, b.i, b.j);
+    var d = abs(a.i-b.i) +abs(a.j-b.j);
+    return d;
+}
+
+cols = 18;
+rows = 18;
 
 var grid = new Array(cols);
 
@@ -15,6 +22,7 @@ var openSet = [];
 var closedSet = [];
 var start;
 var end;
+var path = [];
 
 var w, h;
 
@@ -25,28 +33,29 @@ function Spot(i, j) {
     this.i = i;
     this.j = j;
     this.neighbours = [];
+    this.previous = undefined;
 
 
     this.show = function(col) {
         fill(col);
         noStroke();
-        rect(this.i * w, this.j * h, w - 1, h - 1);
+        rect(this.i * w+1, this.j * h+1, w - 2, h - 2);
     }
 
     this.addNeighbours = function(grid) {
         this.i = i;
         this.j = j;
         if (i < cols - 1) {
-            this.neighbours.push(grid[i + 1, j]);
+            this.neighbours.push(grid[i + 1][j]);
         }
         if (i > 0) {
-            this.neighbours.push(grid[i - 1, j]);
+            this.neighbours.push(grid[i - 1][j]);
         }
         if (j < rows - 1) {
-            this.neighbours.push(grid[i, j + 1]);
+            this.neighbours.push(grid[i][j + 1]);
         }
         if (j > 0) {
-            this.neighbours.push(grid[i, j - 1]);
+            this.neighbours.push(grid[i][j - 1]);
         }
     }
 }
@@ -74,20 +83,17 @@ function setup() {
             grid[i][j].addNeighbours(grid);
         }
     }
-    start = grid[0][0];
-    end = grid[cols - 1][rows - 1];
+    start = grid[3][0];
+    end = grid[cols - 4][rows - 3];
 
     openSet.push(start);
-
-
-
 }
 
 function draw() {
     background(47);
 
     if (openSet.length > 0) {
-        winner = 0;
+        var winner = 0;
         for (var i = 0; i < openSet.length; i++) {
             if (openSet[i].f < openSet[winner].f) {
                 winner = i;
@@ -96,19 +102,38 @@ function draw() {
         // keep going
         var current = openSet[winner];
         if (current === end) {
+            noLoop();
             console.log("Done!");
         }
 
         removeFromArray(openSet, current);
         closedSet.push(current);
+
+        var neighbours = current.neighbours;
+        for (var i = 0; i < neighbours.length; i++) {
+            var neighbour = neighbours[i];
+            if (!closedSet.includes(neighbour)) {
+                var tempG = current.g + 1;
+                if (openSet.includes(neighbour)) {
+                    if (tempG < neighbour.g) {
+                        neighbour.g = tempG;
+                    }
+                } else {
+                    neighbour.g = tempG;
+                    openSet.push(neighbour);
+                }
+                neighbour.h = heuristic(neighbour, end);
+                neighbour.f = neighbour.g + neighbour.h;
+                neighbour.previous = current;
+            }
+        }
     } else {
         //no solution
     }
 
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
-            grid[i][j].show(255);
-
+            grid[i][j].show(color(255));
         }
     }
     for (var i = 0; i < closedSet.length; i++) {
@@ -118,4 +143,16 @@ function draw() {
         openSet[i].show(color(76, 199, 123));
     }
 
+    // find the path
+    path = [];
+    var temp = current;
+    path.push(temp);
+    while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+    }
+
+    for (var i = 0; i < path.length; i++) {
+        path[i].show(color(76, 123, 199));
+    }
 }
